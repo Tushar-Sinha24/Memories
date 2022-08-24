044df9c7a83c73d2993dc6aae302427b2cb54c09
Tushar <sinhatushar9234@gmail.com>
2022-08-24 12:17:30 +0530

    commit

--- client/src/Api/api.js
+++ client/src/Api/api.js
@@ -2,10 +2,4 @@ const axios = require('axios')
 
 const url ='http://localhost:5000/posts';
 
-export const fetchPosts=()=>axios.get(url).then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-
-    console.log(error);
-  })
+export const fetchPosts=()=>axios.get(url);
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import {provider} from 'react-redux';
+import {createStore , applyMiddleware , compose} from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers'
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
