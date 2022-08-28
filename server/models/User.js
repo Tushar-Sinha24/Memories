fa9bdd13f2a308f9985f3c861d4a75f6a16a7970
Tushar <sinhatushar9234@gmail.com>
2022-08-29 00:47:34 +0530

    commit

--- server/models/User.js
+++ server/models/User.js
@@ -1,22 +1,35 @@
 const mongoose=require('mongoose');
 
-const PostSchema=new mongoose.Schema({
-    title: String,
+const UserSchema=new mongoose.Schema({
 
-    name:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
     },
 
-    createdAt : {
+    email: {
+        type: String,
+        required: [true, 'Please add a email'],
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email']
+    },
+    
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    },
+
+    resetPasswordToken: String,
+
+    resetPasswordExpie: Date,
+
+    createdAt: {
         type: Date,
-        default : Date.now
+        default: Date.now
     }
 });
 
 
-module.exports=mongoose.model('Post',PostSchema);
+module.exports=mongoose.model('User',UserSchema);
