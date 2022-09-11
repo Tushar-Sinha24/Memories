56473bf8df518f75bfc67036cd1f2d704eee98d8
Tushar <sinhatushar9234@gmail.com>
2022-09-11 22:38:52 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from 'react'
+import Navbar from './components/Navbar'
+import Login from "./components/Login";
+
+
 function App() {
   return (
     <>
-      
+      <Router>
+
+        <div className="container">
+        <header>
+          <Navbar />
+        </header>
+        </div>
+        
+        <main>
+          <div className="container">
+            <Routes>
+              <Route path='/login' element={<Login />} />
+            </Routes>
+          </div>
+        </main>
+      </Router>
     </>
   );
 }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './css/navbar.css'
+import {AppBar, Box, Toolbar , Typography , Button} from '@mui/material'
+import {Link , NavLink } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <div>
+      <AppBar sx={{background : 'linear-gradient(90deg, rgba(0,2,28,1) 0%, rgba(10,18,88,1) 54%, rgba(0,104,255,1) 100%)'}}>
+        <Toolbar>
+        <NavLink to='/' className='navLink'><Typography variant="h6">Memories App</Typography></NavLink>
+            <Box display='flex' marginLeft='auto'>
+            <Link exact to='/login' className='navLink'><Button  variant='contained' sx={{margin:1 , borderRadius:10}} color='warning'>Login</Button></Link>
+            <Link exact to='/signup' className='navLink'><Button  variant='contained' sx={{margin:1 , borderRadius:10}} color='warning'>Signup</Button></Link>
+            </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default Navbar
