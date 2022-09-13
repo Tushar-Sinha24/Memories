7a0fc4977072b33d4847c021b5d15ae35f75d477
Tushar <sinhatushar9234@gmail.com>
2022-09-13 17:30:27 +0530

    commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,107 @@
 import React from 'react'
+import './css/card.css'
 
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <div className="grid">
+      <div className="card">
+         <div className="image">
+          <img src="https://images.unsplash.com/photo-1663014958092-51b76d48272d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="asdsdas" />
+         </div>
+         <div className="card-body">
+          <div className="title">
+            <h5>userName</h5>
+            <h2>title</h2>
+          </div>
+          <div className="desc">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, eum.</p>
+          </div>
+          <div className="card-footer">
+            <button>Like</button>
+            <button>Comment</button>
+          </div>
+         </div>
+      </div>
+      
+
+      
+      <div className="card">
+         <div className="image">
+          <img src="https://images.unsplash.com/photo-1663014958092-51b76d48272d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="asdsdas" />
+         </div>
+         <div className="card-body">
+          <div className="title">
+            <h2>title</h2>
+          </div>
+          <div className="desc">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, eum.</p>
+          </div>
+          <div className="card-footer">
+            <button>Like</button>
+            <button>Comment</button>
+          </div>
+         </div>
+      </div>
+      
+
+      
+      <div className="card">
+         <div className="image">
+          <img src="https://images.unsplash.com/photo-1663014958092-51b76d48272d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="asdsdas" />
+         </div>
+         <div className="card-body">
+          <div className="title">
+            <h2>title</h2>
+          </div>
+          <div className="desc">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, eum.</p>
+          </div>
+          <div className="card-footer">
+            <button>Like</button>
+            <button>Comment</button>
+          </div>
+         </div>
+      </div>
+
+      <div className="card">
+         <div className="image">
+          <img src="https://images.unsplash.com/photo-1663014958092-51b76d48272d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="asdsdas" />
+         </div>
+         <div className="card-body">
+          <div className="title">
+            <h2>title</h2>
+          </div>
+          <div className="desc">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, eum.</p>
+          </div>
+          <div className="card-footer">
+            <button>Like</button>
+            <button>Comment</button>
+          </div>
+         </div>
+      </div>
+
+      <div className="card">
+         <div className="image">
+          <img src="https://images.unsplash.com/photo-1663014958092-51b76d48272d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="asdsdas" />
+         </div>
+         <div className="card-body">
+          <div className="title">
+            <h2>title</h2>
+          </div>
+          <div className="desc">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, eum.</p>
+          </div>
+          <div className="card-footer">
+            <button>Like</button>
+            <button>Comment</button>
+          </div>
+         </div>
+      </div>
+
+
+      </div>
     </div>
   )
 }
